379ff3ed054f246400c530c3cc2b9a5858c20690
kenny <kanishkan14@gmail.com>
2018-06-16 13:55:56 +0530

    home

--- meberpage/urls.py
+++ meberpage/urls.py
@@ -17,10 +17,12 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
+from.import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^members/', include('members.urls')),
+        url(r'^$',views.homepage),
 ]
 
 
