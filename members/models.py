c779d5e649eae21706ba7621ab4031b4d002a9e5
kenny <kanishkan14@gmail.com>
2018-06-20 18:10:21 +0530

    adding default image

--- members/models.py
+++ members/models.py
@@ -4,7 +4,7 @@ class Member(models.Model):
     cell = models.CharField(max_length=50)
     name=models.CharField(max_length=100)
     post=models.CharField(max_length=100)
-    pics=models.ImageField(default='media/background.jpg')
+    pics=models.ImageField(default='meberpage/media/background.jpg')
 
     def __str__(self):
         return self.name + '-' +self.cell
